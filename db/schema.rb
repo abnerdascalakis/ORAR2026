245828c13978728a77719e4012f9021bacd3ec8d
Abner Dascalakis <abnerdascalakis@gmail.com>
2026-04-15 11:35:24 -0400

    ajuste index e modalidades

--- db/schema.rb
+++ db/schema.rb
@@ -14,39 +14,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_15_000507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "a_sexos", id: :serial, force: :cascade do |t|
+  create_table "a_sexos", id: :integer, default: -> { "nextval('g_sexos_id_seq'::regclass)" }, force: :cascade do |t|
     t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
     t.string "descricao", limit: 100, null: false
     t.datetime "updated_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }
-  end
 
-  create_table "inscricoes", force: :cascade do |t|
-    t.boolean "aceitou_termos", default: false, null: false
-    t.datetime "created_at", null: false
-    t.date "data_nascimento", null: false
-    t.string "distrito", null: false
-    t.string "email", null: false
-    t.string "nome_participante", null: false
-    t.bigint "sexo_id", null: false
-    t.bigint "sociedade_jovem_id", null: false
-    t.datetime "updated_at", null: false
-    t.index ["sexo_id"], name: "index_inscricoes_on_sexo_id"
-    t.index ["sociedade_jovem_id"], name: "index_inscricoes_on_sociedade_jovem_id"
-  end
-
-  create_table "modalidades", force: :cascade do |t|
-    t.boolean "ativa", default: true, null: false
-    t.datetime "created_at", null: false
-    t.string "descricao"
-    t.string "nome", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sociedade_jovens", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "distrito"
-    t.string "nome", null: false
-    t.datetime "updated_at", null: false
+    t.unique_constraint ["descricao"], name: "g_sexos_descricao_key"
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,7 +33,4 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_15_000507) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "inscricoes", "a_sexos", column: "sexo_id"
-  add_foreign_key "inscricoes", "sociedade_jovens", column: "sociedade_jovem_id"
 end
